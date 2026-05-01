dbb27800f2e7e75d41efbe47faab4f0646d1f587
ujjval <ujjvalchauhan220@gmail.com>
2026-05-01 13:43:53 +0530

    fix missing createTask export

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -10,3 +10,21 @@ exports.getTasks = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+exports.createTask = async (req, res) => {
+  try {
+    const { title } = req.body;
+
+    const [result] = await db.query(
+      "INSERT INTO tasks (title, assigned_to) VALUES (?, ?)",
+      [title, req.user.id]
+    );
+
+    res.json({
+      message: "Task created",
+      taskId: result.insertId
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
