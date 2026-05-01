0677b1c70ea9ecde6108eedcabc122c2fd5ab0a4
ujjval <ujjvalchauhan220@gmail.com>
2026-05-01 16:33:38 +0530

    fix top level await error

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,3 @@
-const db = require("../db");          // ✅ correct path
-const bcrypt = require("bcryptjs");  // ✅ correct
-const jwt = require("jsonwebtoken"); // ✅ correct
-
 exports.register = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -15,39 +11,7 @@ exports.register = async (req, res) => {
 
     res.json({ message: "User registered ✅" });
   } catch (err) {
-  console.log("ERROR:", err);   // 👈 terminal में full error दिखेगा
-  res.status(500).json({ error: err });
-
-  }
-};
-
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const [rows] = await db.query(
-      "SELECT * FROM users WHERE email = ?",
-      [email]
-    );
-
-    if (rows.length === 0) {
-      return res.status(400).json({ message: "User not found" });
-    }
-
-    const user = rows[0];
-
-    const valid = await bcrypt.compare(password, user.password);
-
-    if (!valid) {
-      return res.status(400).json({ message: "Invalid password" });
-    }
-
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-
-    res.json({ token });
-
-  } catch (err) {
     console.log("ERROR:", err);
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ error: err });
   }
 };
